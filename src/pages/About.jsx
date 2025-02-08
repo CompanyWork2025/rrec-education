2db0e5300f6d68a811d3327e69b7ce88198b7b39
Deepak Kumar <officewebdeveloper2025@gmail.com>
2025-02-08 10:18:51 +0530

    meta updated for about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Hero from '../components/About/Hero.jsx';
 import Intro from '../components/About/Intro.jsx';
 import WhyChoose from '../components/About/WhyChoose.jsx';
@@ -6,9 +7,15 @@ import WhyChoose from '../components/About/WhyChoose.jsx';
 const About = () => {
   return (
     <>
-     <Hero />
-     <Intro />
-     <WhyChoose />
+      <Helmet>
+        <title>About Us - Study in Russia for indian students</title>
+        <meta name="description" content="Learn more about studying in Russia, the benefits of Russian education, and why you should choose us for your admission process." />
+        <meta name="keywords" content="Study in Russia, Russian Universities, Admission Process, Education in Russia" />
+        <meta name="author" content="RREC Education" />
+      </Helmet>
+      <Hero />
+      <Intro />
+      <WhyChoose />
     </>
   );
 };
